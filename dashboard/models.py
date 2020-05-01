ef40c9cfd41974066e33c7c3ac191befc4529c3d
Evgeniy <pivenevgeniy@ukr.net>
2020-05-01 22:14:00 +0300

    (upd) change float to char coordinate models fields

--- dashboard/models.py
+++ dashboard/models.py
@@ -15,10 +15,10 @@ class Language(models.Model):
 
 class Map(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    highlight_x = models.FloatField(default=0)
-    highlight_y = models.FloatField(default=0)
-    pin_x = models.FloatField(default=0)
-    pin_y = models.FloatField(default=0)
+    highlight_x = models.CharField(max_length=200)
+    highlight_y = models.CharField(max_length=200)
+    pin_x = models.CharField(max_length=200)
+    pin_y = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -31,8 +31,8 @@ class Map(models.Model):
 
 class GeneralMap(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    map_x = models.FloatField(default=0)
-    map_y = models.FloatField(default=0)
+    map_x = models.CharField(max_length=200)
+    map_y = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -141,8 +141,8 @@ class Museums(models.Model):
 
 class Route(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    route_x = models.FloatField(default=0)
-    route_y = models.FloatField(default=0)
+    route_x = models.CharField(max_length=200)
+    route_y = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
