620dbefd195083e6291dffe5edb91e632b31c6e1
pivenevgeniy <pivenevgeniy@ukr.net>
2021-04-27 16:03:05 +0300

    (upd) remove audio text field for last migration

--- dashboard/migrations/0008_auto_20210427_1556.py
+++ dashboard/migrations/0008_auto_20210427_1556.py
@@ -19,12 +19,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='articles',
-            name='audio_text',
-            field=models.FileField(default=False, upload_to='media/audio/', validators=[dashboard.validators.validate_music_file_extension]),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='articles',
             name='brief_description_language',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='article_brief_description_language', to='dashboard.Language', verbose_name='Article Brief Description Language'),
         ),
